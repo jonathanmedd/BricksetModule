b8a83c438b90148c1e9ae3b8af975b527df09e53
Jonathan Medd <jon_medd@hotmail.com>
2016-11-15 23:17:22 +0000

    Initial drafts for Set-BricksetCollectionSetWanted and Set-BricksetCollectionSetUnwanted

---
+++ Brickset/Functions/Public/Set-BricksetCollectionSetUnwanted.psm1
@@ -0,0 +1,49 @@
+﻿function Set-BricksetCollectionSetUnwanted {
+<#
+    .SYNOPSIS
+    Set a Brickset Set to Unwanted status
+    
+    .DESCRIPTION
+    Set a Brickset Set to Unwanted status
+
+    .PARAMETER SetId
+    Brickset SetId
+
+    .INPUTS
+    String
+
+    .OUTPUTS
+    None
+
+    .EXAMPLE
+    Set-BricksetCollectionSetUnwanted -SetId 26049
+#>
+[CmdletBinding(SupportsShouldProcess,ConfirmImpact="High")]
+
+    Param
+    (
+    [parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+    [ValidateNotNullOrEmpty()]
+    [String]$SetId
+
+    )
+    
+    try {
+
+        # --- Check for the presence of $Global:BricksetConnection
+        xCheckGlobalBricksetConnection
+
+        # --- Check for the UserHash
+        xCheckUserHash
+
+        # --- Make the Webservice Call
+        if ($PSCmdlet.ShouldProcess($SetId)){
+
+            $BricksetConnection.WebService.setCollection_wants($BricksetConnection.APIKey, $BricksetConnection.UserHash, $SetId, 0)
+        }
+    }
+    catch [Exception]{
+            
+        throw
+    }
+}
