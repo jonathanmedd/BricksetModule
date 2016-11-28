6c0d0a02b39a72ea41329c87cdd7f6e0cbdbf620
Jonathan Medd <jon_medd@hotmail.com>
2016-11-28 17:37:21 +0000

    Initial draft for Get-BricksetCollectionUserNotes

---
+++ Brickset/Functions/Public/Get-BricksetCollectionUserNotes.psm1
@@ -0,0 +1,40 @@
+﻿function Get-BricksetCollectionUserNotes {
+<#
+    .SYNOPSIS
+    Get all a user's set notes.
+    
+    .DESCRIPTION
+    Get all a user's set notes.
+
+    .INPUTS
+    None
+
+    .OUTPUTS
+    Brickset.userNotes
+
+    .EXAMPLE
+    Get-BricksetCollectionUserNotes
+#>
+[CmdletBinding()][OutputType('Brickset.userNotes')]
+
+    Param
+    (
+
+    )
+    
+    try {
+
+        # --- Check for the presence of $Global:BricksetConnection
+        xCheckGlobalBricksetConnection
+    
+        # --- Check for the UserHash
+        xCheckUserHash
+
+        # --- Make the Webservice Call
+        $BricksetConnection.WebService.getUserNotes($BricksetConnection.APIKey,$BricksetConnection.UserHash)
+    }
+    catch [Exception]{
+            
+        throw
+    }
+}
