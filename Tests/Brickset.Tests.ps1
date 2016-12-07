020f2080dd33faa12d5402312abfa56e14c4adb7
Jonathan Medd <jon_medd@hotmail.com>
2016-12-07 23:54:10 +0000

    Initial thoughts

---
+++ Tests/Brickset.Tests.ps1
@@ -0,0 +1,25 @@
+﻿Describe 'First' {
+
+    It 'getYears' {
+
+
+
+        $Global:BricksetConnection = @{ }
+        $Global:BricksetConnection.WebService = [PSCustomObject]@{} | Add-Member -Passthru ScriptMethod getYears { 
+
+               [PSCustomObject]@{
+
+                theme = "Star Wars"
+                year = "1999"
+                setCount = 13
+
+                }
+
+            }
+
+
+
+        $theme = Get-BricksetYear -Theme 'Star Wars' | Select-Object -ExpandProperty theme
+        $theme | Should Be 'Star'
+    }
+}
