fbdc293a285aa4ec61f57c5bfbca2f4af8d7fa59
Jonathan Medd <jon_medd@hotmail.com>
2016-12-19 12:14:48 +0000

    Updated tests

--- Tests/Brickset.Tests.ps1
+++ Tests/Brickset.Tests.ps1
@@ -1,25 +1,75 @@
-﻿Describe 'First' {
+﻿# --- Build out Brickset Connection for tests
+$Global:BricksetConnection = @{ }
+$Global:BricksetConnection.UserHash = 'sdfjhg23'
+$Global:BricksetConnection.WebService = [PSCustomObject]@{} | Add-Member -Passthru ScriptMethod getCollectionTotals {
 
-    It 'getYears' {
+    [PSCustomObject]@{
+
+        totalSetsOwned = 3
+        totalDistinctSetsOwned = 3
+        totalSetsWanted = 13
+        totalMinifigsOwned = 5
+        totalMinifigsWanted = 0
+    }
+}
+$Global:BricksetConnection.WebService | Add-Member -Passthru ScriptMethod getUserNotes {
+
+    [PSCustomObject]@{
+
+        setID = "26049"
+        userNotes1 = "testing"
+    }
+}
+$Global:BricksetConnection.WebService | Add-Member -Passthru ScriptMethod getMinifigCollection {
+
+    [PSCustomObject]@{
+
+        minifigNumber = "sw714"
+        ownedInSets = 0
+        ownedLoose = 0
+        ownedTotal = 0
+        wanted = "True"
+    }
+}
+$Global:BricksetConnection.WebService | Add-Member -Passthru ScriptMethod getYears {
+
+    [PSCustomObject]@{
+
+        theme = "Star Wars"
+        year = "1999"
+        setCount = 13
+    }
+}
 
 
 
-        $Global:BricksetConnection = @{ }
-        $Global:BricksetConnection.WebService = [PSCustomObject]@{} | Add-Member -Passthru ScriptMethod getYears { 
 
-               [PSCustomObject]@{
+# --- Tests
+Describe 'Testing Get Functions' {
 
-                theme = "Star Wars"
-                year = "1999"
-                setCount = 13
+    It 'Testing Get-BricksetCollectionTotals' {
 
-                }
+        $totalSetsOwned = Get-BricksetCollectionTotals | Select-Object -ExpandProperty totalSetsOwned
+        $totalSetsOwned | Should Be 3
+    }
+    It 'Testing Get-BricksetCollectionUserNotes' {
 
-            }
+        $setID = Get-BricksetCollectionUserNotes | Select-Object -ExpandProperty setID
+        $setID | Should Be "26049"
+    }
+    It 'Testing Get-BricksetMinifigCollectionOwned' {
 
+        $minifigNumber = Get-BricksetMinifigCollectionOwned | Select-Object -ExpandProperty minifigNumber
+        $minifigNumber | Should Be "sw714"
+    }
+    It 'Testing Get-BricksetMinifigCollectionWanted' {
 
+        $wanted = Get-BricksetMinifigCollectionWanted | Select-Object -ExpandProperty wanted
+        $wanted | Should Be "True"
+    }
+    It 'Testing Get-BricksetYear' {
 
         $theme = Get-BricksetYear -Theme 'Star Wars' | Select-Object -ExpandProperty theme
-        $theme | Should Be 'Star'
+        $theme | Should Be 'Star Wars'
     }
 }
