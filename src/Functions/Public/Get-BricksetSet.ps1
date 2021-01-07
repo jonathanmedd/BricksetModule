9c223586348926d07cf3b3f567d7c09a05584c51
Jonathan Medd <jonathan.medd@atos.net>
2021-01-07 18:20:16 +0000

    Updated Get-BricksetSet

--- src/Functions/Public/Get-BricksetSet.ps1
+++ src/Functions/Public/Get-BricksetSet.ps1
@@ -1,8 +1,8 @@
 ﻿function Get-BricksetSet {
-<#
+    <#
     .SYNOPSIS
     Get Brickset Sets
-    
+
     .DESCRIPTION
     Get Brickset Sets
 
@@ -43,46 +43,84 @@
     .EXAMPLE
     Get-BricksetSet -Theme 'Indiana Jones' -OrderBy Pieces
 #>
-[CmdletBinding()][OutputType('Brickset.sets')]
+    [CmdletBinding()][OutputType('System.Management.Automation.PSObject')]
 
-    Param
-    (
+    Param (
 
-    [parameter(Mandatory=$false)]
-    [ValidateNotNullOrEmpty()]
-    [String]$Theme,
+        [parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [String]$theme,
 
-    [parameter(Mandatory=$false)]
-    [ValidateNotNullOrEmpty()]
-    [String]$Subtheme,
+        [parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [String]$subTheme,
 
-    [parameter(Mandatory=$false)]
-    [ValidateNotNullOrEmpty()]
-    $Year,
+        [parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        $year,
 
-    [parameter(Mandatory=$false)]
-    [ValidateNotNullOrEmpty()]
-    [String]$SetNumber,
+        [parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [String]$setNumber,
 
-    [parameter(Mandatory=$false)]
-    [ValidateSet('Number','YearFrom','Pieces','Minifigs','Rating','UKRetailPrice','USRetailPrice','CARetailPrice','EURetailPrice','Theme','Subtheme','Name','Random')]
-    [String]$OrderBy,
+        [parameter(Mandatory = $false)]
+        [ValidateSet('Number', 'YearFrom', 'Pieces', 'Minifigs', 'Rating', 'UKRetailPrice', 'USRetailPrice', 'CARetailPrice', 'DERetailPrice', 'FRRetailPrice',
+            'UKPricePerPiece', 'USPricePerPiece', 'CAPricePerPiece', 'DEPricePerPiece', 'FRPricePerPiece', 'Theme', 'Subtheme', 'Name', 'Random', 'QtyOwned', 'OwnCount', 'WantCount', 'UserRating', 'CollectionID')]
+        [String]$orderBy,
 
-    [parameter(Mandatory=$false)]
-    [ValidateNotNullOrEmpty()]
-    [Int]$PageSize = 1000
+        [parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [Int]$pageSize = 500
     )
-    
+
     try {
 
         # --- Check for the presence of $Global:BricksetConnection
         xCheckGlobalBricksetConnection
 
-        # --- Make the Webservice Call
-        $BricksetConnection.WebService.getSets($BricksetConnection.APIKey,$null,$null,$Theme,$Subtheme,$SetNumber,$Year,$null,$null,$OrderBy,$PageSize,$null,$null)
+        # - Prepare the JSON params
+        $jsonParams = [PSCustomObject] @{}
+
+        if ($PSBoundParameters.ContainsKey('theme')) {
+
+            $jsonParams | Add-Member -MemberType NoteProperty -Name 'theme' -Value $theme
+        }
+        if ($PSBoundParameters.ContainsKey('subTheme')) {
+
+            $jsonParams | Add-Member -MemberType NoteProperty -Name 'subtheme' -Value $subTheme
+        }
+        if ($PSBoundParameters.ContainsKey('year')) {
+
+            $jsonParams | Add-Member -MemberType NoteProperty -Name 'year' -Value $year
+        }
+        if ($PSBoundParameters.ContainsKey('setNumber')) {
+
+            $jsonParams | Add-Member -MemberType NoteProperty -Name 'setNumber' -Value $setNumber
+        }
+        if ($PSBoundParameters.ContainsKey('orderBy')) {
+
+            $jsonParams | Add-Member -MemberType NoteProperty -Name 'orderBy' -Value $orderBy
+        }
+
+        $stringParam = $jsonParams | ConvertTo-Json -Compress
+
+        Write-Verbose "jsonParams are: $stringParam"
+
+        # --- Make the REST Call
+        $body = @{
+            apiKey   = $Script:BricksetConnection.apiKey
+            userHash = $null
+            params   = $stringParam
+        }
+
+        Write-Verbose "Body is: $($body | ConvertTo-Json -Depth 5)"
+
+        $response = Invoke-BricksetRestMethod -Method POST -URI '/getSets' -Body $body
+
+        $response.sets
     }
-    catch [Exception]{
-            
+    catch [Exception] {
+
         throw
     }
 }
