ba378dfa15546e8aa855e8a090d3e491ab56a6c8
Jonathan Medd <jonathan.medd@atos.net>
2021-01-08 11:23:04 +0000

    Updated Get-BricksetSetOwned

--- src/Functions/Public/Get-BricksetSetOwned.ps1
+++ src/Functions/Public/Get-BricksetSetOwned.ps1
@@ -1,8 +1,8 @@
 ﻿function Get-BricksetSetOwned {
-<#
+    <#
     .SYNOPSIS
     Get Brickset Sets Owned
-    
+
     .DESCRIPTION
     Get Brickset Sets Owned
 
@@ -17,38 +17,64 @@
     System.Int.
 
     .OUTPUTS
-    Brickset.sets
+    System.Management.Automation.PSObject
 
     .EXAMPLE
     Get-BricksetSetOwned -OrderBy Pieces
 #>
-[CmdletBinding()][OutputType('Brickset.sets')]
+    [CmdletBinding()][OutputType('System.Management.Automation.PSObject')]
 
     Param
     (
+        [parameter(Mandatory = $false)]
+        [ValidateSet('Number', 'YearFrom', 'Pieces', 'Minifigs', 'Rating', 'UKRetailPrice', 'USRetailPrice', 'CARetailPrice', 'DERetailPrice', 'FRRetailPrice',
+            'UKPricePerPiece', 'USPricePerPiece', 'CAPricePerPiece', 'DEPricePerPiece', 'FRPricePerPiece', 'Theme', 'Subtheme', 'Name', 'Random', 'QtyOwned', 'OwnCount', 'WantCount', 'UserRating', 'CollectionID')]
+        [String]$orderBy,
 
-    [parameter(Mandatory=$false)]
-    [ValidateSet('Number','YearFrom','Pieces','Minifigs','Rating','UKRetailPrice','USRetailPrice','CARetailPrice','EURetailPrice','Theme','Subtheme','Name','Random')]
-    [String]$OrderBy,
-
-    [parameter(Mandatory=$false)]
-    [ValidateNotNullOrEmpty()]
-    [Int]$PageSize = 1000
+        [parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [Int]$pageSize = 500
     )
-    
+
     try {
 
-        # --- Check for the presence of $Global:BricksetConnection
+        # --- Check for the presence of $Script:BricksetConnection
         xCheckGlobalBricksetConnection
-    
+
         # --- Check for the UserHash
         xCheckUserHash
 
-        # --- Make the Webservice Call
-        $BricksetConnection.WebService.getSets($BricksetConnection.APIKey,$BricksetConnection.UserHash,$null,$null,$null,$null,$null,1,$null,$OrderBy,$PageSize,$null,$null)
+        # - Prepare the JSON params
+        $jsonParams = [PSCustomObject] @{
+
+            owned    = '1'
+            pageSize = $pageSize
+        }
+
+        if ($PSBoundParameters.ContainsKey('orderBy')) {
+
+            $jsonParams | Add-Member -MemberType NoteProperty -Name 'orderBy' -Value $orderBy
+        }
+
+        $stringParam = $jsonParams | ConvertTo-Json -Compress
+
+        Write-Verbose "jsonParams are: $stringParam"
+
+        # --- Make the REST Call
+        $body = @{
+            apiKey   = $Script:BricksetConnection.apiKey
+            userHash = $Script:BricksetConnection.userHash
+            params   = $stringParam
+        }
+
+        Write-Verbose "Body is: $($body | ConvertTo-Json -Depth 5)"
+
+        $response = Invoke-BricksetRestMethod -Method POST -URI '/getSets' -Body $body
+
+        $response.sets
     }
-    catch [Exception]{
-            
+    catch [Exception] {
+
         throw
     }
 }
