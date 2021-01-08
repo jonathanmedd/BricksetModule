0ad4394df5cf6b75bd583729cc9a32e7dd6b86e3
Jonathan Medd <jonathan.medd@atos.net>
2021-01-08 14:55:31 +0000

    Updated Get-BricksetSetInstructions

--- src/Functions/Public/Get-BricksetSetInstructions.ps1
+++ src/Functions/Public/Get-BricksetSetInstructions.ps1
@@ -1,19 +1,19 @@
 ﻿function Get-BricksetSetInstructions {
-<#
+    <#
     .SYNOPSIS
     Get Brickset Set Instructions
-    
+
     .DESCRIPTION
     Get Brickset Set Instructions
-    
-    .PARAMETER SetId
+
+    .PARAMETER setId
     Brickset SetId (not the Lego Set Number)
 
     .INPUTS
     System.String.
 
     .OUTPUTS
-    Brickset.instructions
+    System.Management.Automation.PSObject
 
     .EXAMPLE
     Get-BricksetSetInstructions -SetId 6905
@@ -22,19 +22,18 @@
     Get-BricksetSet -Theme 'Indiana Jones' | Get-BricksetSetInstructions
 
     .EXAMPLE
-    Get-BricksetSet -SetNumber 75055-1 | Get-BricksetSetInstructions | Select-Object -First 1 -ExpandProperty url | Foreach-Object {Invoke-Expression “cmd.exe /C start $_”}
+    Get-BricksetSet -SetNumber 75055-1 | Get-BricksetSetInstructions | Select-Object -First 1 -ExpandProperty url | Foreach-Object {Invoke-Expression 'cmd.exe /C start $_'}
 
     .EXAMPLE
     Get-BricksetSet -Theme 'Indiana Jones' | Get-BricksetSetInstructions | Select-Object -ExpandProperty url | ForEach-Object {$file = ($_.split('/'))[-1]; Start-BitsTransfer -Source $_ -Destination "C:\Users\jmedd\Documents\$file"}
 #>
-[CmdletBinding()][OutputType('Brickset.instructions')]
+    [CmdletBinding()][OutputType('System.Management.Automation.PSObject')]
 
     Param
     (
-
-    [parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true)]
-    [ValidateNotNullOrEmpty()]
-    [String]$SetId
+        [parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$setId
     )
 
     begin {
@@ -43,13 +42,23 @@
         xCheckGlobalBricksetConnection
     }
     process {
-        
+
         try {
-        
-            $BricksetConnection.WebService.getInstructions($BricksetConnection.APIKey,$SetId)
+
+            # --- Make the REST Call
+            $body = @{
+                apiKey = $Script:BricksetConnection.apiKey
+                setId  = $setId
+            }
+
+            Write-Verbose "Body is: $($body | ConvertTo-Json -Depth 5)"
+
+            $response = Invoke-BricksetRestMethod -Method POST -URI '/getInstructions' -Body $body
+
+            $response.instructions
         }
-        catch [Exception]{
-            
+        catch [Exception] {
+
             throw
         }
     }
