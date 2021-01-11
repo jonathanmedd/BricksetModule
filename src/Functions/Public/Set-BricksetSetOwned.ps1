d779847488dcd88d7b21bfa128b96f6e4d674b7f
Jonathan Medd <jonathan.medd@atos.net>
2021-01-11 16:04:33 +0000

    Updated Set-BricksetSetOwned

--- src/Functions/Public/Set-BricksetSetOwned.ps1
+++ src/Functions/Public/Set-BricksetSetOwned.ps1
@@ -1,8 +1,8 @@
 ﻿function Set-BricksetSetOwned {
-<#
+    <#
     .SYNOPSIS
     Set a Brickset Set to Owned status
-    
+
     .DESCRIPTION
     Set a Brickset Set to Owned status. Include the number of sets owned.
 
@@ -25,20 +25,23 @@
     .EXAMPLE
     Get-BricksetSet -SetNumber '7199-1' | Set-BricksetSetOwned -QtyOwned 1 -Confirm:$false
 #>
-[CmdletBinding(SupportsShouldProcess,ConfirmImpact="High")]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
 
-    Param
+    param
     (
-    [parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
-    [ValidateNotNullOrEmpty()]
-    [String]$SetId,
+        [parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$setId,
 
-    [parameter(Mandatory=$true)]
-    [ValidateNotNullOrEmpty()]
-    [Int]$QtyOwned
+        [parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [Int]$qtyOwned,
 
+        [parameter(Mandatory = $false)]
+        [ValidateSet(1, 2, 3, 4, 5)]
+        [Int]$rating
     )
-    
+
     try {
 
         # --- Check for the presence of $Global:BricksetConnection
@@ -47,14 +50,39 @@
         # --- Check for the UserHash
         xCheckUserHash
 
-        # --- Make the Webservice Call
-        if ($PSCmdlet.ShouldProcess($SetId)){
+        if ($PSCmdlet.ShouldProcess($setId)) {
 
-            $BricksetConnection.WebService.setCollection($BricksetConnection.APIKey, $BricksetConnection.UserHash, $SetId, $QtyOwned, 0)
+            # - Prepare the JSON params
+            $jsonParams = [PSCustomObject] @{
+
+                own      = '1'
+                qtyOwned = $qtyOwned
+            }
+
+            if ($PSBoundParameters.ContainsKey('rating')) {
+
+                $jsonParams | Add-Member -MemberType NoteProperty -Name 'rating' -Value $rating
+            }
+
+            $stringParam = $jsonParams | ConvertTo-Json -Compress
+
+            Write-Verbose "jsonParams are: $stringParam"
+
+            # --- Make the REST Call
+            $body = @{
+                apiKey   = $Script:BricksetConnection.apiKey
+                userHash = $Script:BricksetConnection.userHash
+                params   = $stringParam
+                setID    = $setId
+            }
+
+            Write-Verbose "Body is: $($body | ConvertTo-Json -Depth 5)"
+
+            Invoke-BricksetRestMethod -Method POST -URI '/setCollection' -Body $body
         }
     }
-    catch [Exception]{
-            
+    catch [Exception] {
+
         throw
     }
 }
