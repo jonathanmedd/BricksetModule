638e009f594baf15439909c2cc096343d78c50b4
Jonathan Medd <jonathan.medd@atos.net>
2021-01-11 11:56:39 +0000

    Updated Set-BricksetMinifgCollectionOwned

--- src/Functions/Public/Set-BricksetMinifgCollectionOwned.ps1
+++ src/Functions/Public/Set-BricksetMinifgCollectionOwned.ps1
@@ -1,8 +1,8 @@
 ﻿function Set-BricksetMinifgCollectionOwned {
-<#
+    <#
     .SYNOPSIS
     Set a Brickset Minifg to Owned status
-    
+
     .DESCRIPTION
     Set a Brickset Minfig to Owned status. Include the number of sets owned.
 
@@ -22,20 +22,19 @@
     .EXAMPLE
     Set-BricksetMinifgCollectionOwned -MinifgNumber sw705 -QtyOwned 1
 #>
-[CmdletBinding(SupportsShouldProcess,ConfirmImpact="High")]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
 
     Param
     (
-    [parameter(Mandatory=$true)]
-    [ValidateNotNullOrEmpty()]
-    [String]$MinifgNumber,
+        [parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$minifigNumber,
 
-    [parameter(Mandatory=$true)]
-    [ValidateNotNullOrEmpty()]
-    [Int]$QtyOwned
-
+        [parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [Int]$qtyOwned
     )
-    
+
     try {
 
         # --- Check for the presence of $Global:BricksetConnection
@@ -44,14 +43,34 @@
         # --- Check for the UserHash
         xCheckUserHash
 
-        # --- Make the Webservice Call
-        if ($PSCmdlet.ShouldProcess($MinifgNumber)){
+        if ($PSCmdlet.ShouldProcess($minifigNumber)) {
 
-            $BricksetConnection.WebService.setMinifigCollection($BricksetConnection.APIKey, $BricksetConnection.UserHash, $MinifgNumber, $QtyOwned, 0)
+            # - Prepare the JSON params
+            $jsonParams = [PSCustomObject] @{
+
+                owned = '1'
+                qtyOwned = $qtyOwned
+            }
+
+            $stringParam = $jsonParams | ConvertTo-Json -Compress
+
+            Write-Verbose "jsonParams are: $stringParam"
+
+            # --- Make the REST Call
+            $body = @{
+                apiKey   = $Script:BricksetConnection.apiKey
+                userHash = $Script:BricksetConnection.userHash
+                params   = $stringParam
+                minifigNumber = $minifigNumber
+            }
+
+            Write-Verbose "Body is: $($body | ConvertTo-Json -Depth 5)"
+
+            Invoke-BricksetRestMethod -Method POST -URI '/setMinifigCollection' -Body $body
         }
     }
-    catch [Exception]{
-            
+    catch [Exception] {
+
         throw
     }
 }
