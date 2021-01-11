1e156b19cb78206bf7101c59b02c340e2585f707
Jonathan Medd <jonathan.medd@atos.net>
2021-01-11 16:47:21 +0000

    Updated Set-BricksetSetWanted

--- src/Functions/Public/Set-BricksetSetWanted.ps1
+++ src/Functions/Public/Set-BricksetSetWanted.ps1
@@ -1,8 +1,8 @@
 ﻿function Set-BricksetSetWanted {
-<#
+    <#
     .SYNOPSIS
     Set a Brickset Set to Wanted status
-    
+
     .DESCRIPTION
     Set a Brickset Set to Wanted status
 
@@ -18,32 +18,50 @@
     .EXAMPLE
     Set-BricksetSetWanted -SetId 26049
 #>
-[CmdletBinding(SupportsShouldProcess,ConfirmImpact="High")]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
 
-    Param
+    param
     (
-    [parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
-    [ValidateNotNullOrEmpty()]
-    [String]$SetId
-
+        [parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$setId
     )
-    
+
     try {
 
-        # --- Check for the presence of $Global:BricksetConnection
+        # --- Check for the presence of $Script:BricksetConnection
         xCheckGlobalBricksetConnection
 
         # --- Check for the UserHash
         xCheckUserHash
 
-        # --- Make the Webservice Call
-        if ($PSCmdlet.ShouldProcess($SetId)){
+        if ($PSCmdlet.ShouldProcess($setId)) {
 
-            $BricksetConnection.WebService.setCollection_wants($BricksetConnection.APIKey, $BricksetConnection.UserHash, $SetId, 1)
+            # - Prepare the JSON params
+            $jsonParams = [PSCustomObject] @{
+
+                want = '1'
+            }
+
+            $stringParam = $jsonParams | ConvertTo-Json -Compress
+
+            Write-Verbose "jsonParams are: $stringParam"
+
+            # --- Make the REST Call
+            $body = @{
+                apiKey   = $Script:BricksetConnection.apiKey
+                userHash = $Script:BricksetConnection.userHash
+                params   = $stringParam
+                setID    = $setId
+            }
+
+            Write-Verbose "Body is: $($body | ConvertTo-Json -Depth 5)"
+
+            Invoke-BricksetRestMethod -Method POST -URI '/setCollection' -Body $body
         }
     }
-    catch [Exception]{
-            
+    catch [Exception] {
+
         throw
     }
 }
