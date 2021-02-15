c34000a16b23170ec243387dd6f31af6ac3a6ce7
Jonathan Medd <jon_medd@hotmail.com>
2021-02-15 16:20:49 +0000

    Added process block

--- src/Functions/Public/Get-BricksetSet.ps1
+++ src/Functions/Public/Get-BricksetSet.ps1
@@ -73,57 +73,63 @@
         [Int]$pageSize = 500
     )
 
-    try {
+    begin {
 
         # --- Check for the presence of $Script:BricksetConnection
         xCheckScriptBricksetConnection
-
-        # - Prepare the JSON params
-        $jsonParams = [PSCustomObject] @{
-
-            pageSize = $pageSize
-        }
-
-        if ($PSBoundParameters.ContainsKey('theme')) {
-
-            $jsonParams | Add-Member -MemberType NoteProperty -Name 'theme' -Value $theme
-        }
-        if ($PSBoundParameters.ContainsKey('subTheme')) {
-
-            $jsonParams | Add-Member -MemberType NoteProperty -Name 'subtheme' -Value $subTheme
-        }
-        if ($PSBoundParameters.ContainsKey('year')) {
-
-            $jsonParams | Add-Member -MemberType NoteProperty -Name 'year' -Value $year
-        }
-        if ($PSBoundParameters.ContainsKey('setNumber')) {
-
-            $jsonParams | Add-Member -MemberType NoteProperty -Name 'setNumber' -Value $setNumber
-        }
-        if ($PSBoundParameters.ContainsKey('orderBy')) {
-
-            $jsonParams | Add-Member -MemberType NoteProperty -Name 'orderBy' -Value $orderBy
-        }
-
-        $stringParam = $jsonParams | ConvertTo-Json -Compress
-
-        Write-Verbose "jsonParams are: $stringParam"
-
-        # --- Make the REST Call
-        $body = @{
-            apiKey   = $Script:BricksetConnection.apiKey
-            userHash = $null
-            params   = $stringParam
-        }
-
-        Write-Verbose "Body is: $($body | ConvertTo-Json -Depth 5)"
-
-        $response = Invoke-BricksetRestMethod -Method POST -URI '/getSets' -Body $body
-
-        $response.sets
     }
-    catch [Exception] {
 
-        throw
+    process {
+
+        try {
+
+            # - Prepare the JSON params
+            $jsonParams = [PSCustomObject] @{
+
+                pageSize = $pageSize
+            }
+
+            if ($PSBoundParameters.ContainsKey('theme')) {
+
+                $jsonParams | Add-Member -MemberType NoteProperty -Name 'theme' -Value $theme
+            }
+            if ($PSBoundParameters.ContainsKey('subTheme')) {
+
+                $jsonParams | Add-Member -MemberType NoteProperty -Name 'subtheme' -Value $subTheme
+            }
+            if ($PSBoundParameters.ContainsKey('year')) {
+
+                $jsonParams | Add-Member -MemberType NoteProperty -Name 'year' -Value $year
+            }
+            if ($PSBoundParameters.ContainsKey('setNumber')) {
+
+                $jsonParams | Add-Member -MemberType NoteProperty -Name 'setNumber' -Value $setNumber
+            }
+            if ($PSBoundParameters.ContainsKey('orderBy')) {
+
+                $jsonParams | Add-Member -MemberType NoteProperty -Name 'orderBy' -Value $orderBy
+            }
+
+            $stringParam = $jsonParams | ConvertTo-Json -Compress
+
+            Write-Verbose "jsonParams are: $stringParam"
+
+            # --- Make the REST Call
+            $body = @{
+                apiKey   = $Script:BricksetConnection.apiKey
+                userHash = $null
+                params   = $stringParam
+            }
+
+            Write-Verbose "Body is: $($body | ConvertTo-Json -Depth 5)"
+
+            $response = Invoke-BricksetRestMethod -Method POST -URI '/getSets' -Body $body
+
+            $response.sets
+        }
+        catch [Exception] {
+
+            throw
+        }
     }
 }
